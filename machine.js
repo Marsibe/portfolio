5b96d6ff25db7c80f558081d70e42cf70b8856a7
Marsibe <simoes.marcos@hotmail.com>
2022-07-15 18:54:24 -0300

    Seção header iniciada

---
+++ machine.js
@@ -0,0 +1,13 @@
+function typeWriter(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) =>{
+        setTimeout(() =>{
+            elemento.innerHTML += letra;
+        }, 250 * i);
+    });
+}
+
+const titulo = document.querySelector("#subtitle");
+typeWriter(titulo);
+
